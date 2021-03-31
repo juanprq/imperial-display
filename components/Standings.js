7d5dbf55a2ced5f8ffa92efffc97a5ee9a26bcc8
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-03-31 15:37:33 -0500

    adds progress in the standings layout

--- components/Standings.js
+++ components/Standings.js
@@ -3,9 +3,23 @@ import styles from './Standings.module.css';
 const Standings = ({ players }) => (
   <div>
     <h2>Standings</h2>
-    {players.map((player) => (
-      <div key={player.nick}>{player.nick}</div>
-    ))}
+    <div className={styles.container}>
+      {players.map((player) => (
+        <div key={player.nick} className={styles.player}>
+          <div className={styles.avatar}>
+            <img src={player.avatar} alt={`${player.nick}_avatar`} />
+          </div>
+          <div>
+            <div>
+              {player.name} <span>{player.alias}</span>
+            </div>
+            <div>
+              15
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 );
 
