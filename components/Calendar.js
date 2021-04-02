2e2bf9b3ecd1d01a51b4146ec340c7b59c1baba2
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-04-01 23:50:57 -0500

    adds calendar component

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,24 +1,43 @@
 import RCalendar from 'rc-year-calendar';
+import { useCallback } from 'react';
+import { google } from 'calendar-link';
 
 import styles from './Calendar.module.css';
 
-const Calendar = ({ data }) => (
-  <div>
-    <h2>Calendario de eventos</h2>
-    <div className={styles.container}>
-      <RCalendar
-        style="background"
-        onDayClick={({ date }) => {
-          console.log(date);
-        }}
-        dataSource={data.map((data) => ({
-          ...data,
-          startDate: new Date(Date.parse(data.startDate, 'dd/MM/yyyy')),
-          endDate: new Date(Date.parse(data.endDate, 'dd/MM/yyyy')),
-        }))}
-      />
+const Calendar = ({ data }) => {
+  const onDayClick = useCallback(
+    ({ events }) => {
+      const [event] = events;
+      if (event) {
+        const url = google({
+          title: event.name,
+          description: 'A jugar Twilight Imperium!!',
+          start: event.startDate.toISOString(),
+          duration: [6, 'hour'],
+        });
+
+        window.open(url, '_blank').focus();
+      }
+    },
+    [data]
+  );
+
+  return (
+    <div>
+      <h2>Calendario de eventos</h2>
+      <div className={styles.container}>
+        <RCalendar
+          style="background"
+          onDayClick={onDayClick}
+          dataSource={data.map((data) => ({
+            ...data,
+            startDate: new Date(data.startDate),
+            endDate: new Date(data.endDate),
+          }))}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calendar;
