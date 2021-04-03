fad8e87c23c82a4029ba396a7e3a81ba8c1bec9a
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-04-03 17:49:22 -0500

    fix to lowercase the next/head import

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/Head';
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import Rules from '../components/Rules';
 import Standings from '../components/Standings';
